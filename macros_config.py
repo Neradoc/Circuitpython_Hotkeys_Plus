8d0152d06086d89a53c07bed4339e318f0b31d3e
Neradoc <neraOnGit@ri1.fr>
2021-09-08 19:44:21 +0200

    update to latest layout format

--- macros_config.py
+++ macros_config.py
@@ -3,7 +3,7 @@ Setup the Keycode and Layout modules to use in the macros.
 default_layout = ...
 default_keycode = ...
 """
-# from keyboard_layout_mac_fr import KeyboardLayoutMacFr
-# default_layout = KeyboardLayoutMacFr
+# from keyboard_layout_mac_fr import KeyboardLayout
+# default_layout = KeyboardLayout
 # from keycode_mac_fr import Keycode
 # default_keycode = Keycode
