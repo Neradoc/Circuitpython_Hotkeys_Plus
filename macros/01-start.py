89e04beeb4f28124c0966d91aa49b6ae10daf459
Neradoc <neraOnGit@ri1.fr>
2021-09-08 21:27:19 +0200

    add more examples in the start page

--- macros/01-start.py
+++ macros/01-start.py
@@ -1,5 +1,5 @@
 # MACROPAD Hotkeys example: Universal Numpad
-from macro_actions import K, L, Tone
+from macro_actions import K, Type, Tone, Control
 import time
 
 def onoff(pad, key, idx):
@@ -21,34 +21,39 @@ def leaving(pad, prev_app, next_app):
 
 app = {                    # REQUIRED dict, must be named 'app'
     'name' : 'Test Macros', # Application name
+    'enter' : entering,
+    'leave' : leaving,
     'macros' : [           # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x202000, '7', K("A")),
+        (0x202000, '7', K("7")),
         (0x202000, '8', K("8")),
         (0x202000, '9', K("9")),
         # 2nd row ----------
-        (0x202000, '4', K("4")),
-        (0x202000, '5', K("5")),
-        (0x202000, '6', K("6")),
+        (0x202000, 'V-', Control("VOLUME_DECREMENT")),
+        (0x202000, 'V+', Control("VOLUME_INCREMENT")),
+        (0x202000, 'Calc', Control(0x192)),
         # 3rd row ----------
-        (0x202000, '1', Tone( ("A4", 0.5) ) ),
-        (0x202000, '2', Tone(
+        (0x202000, 'Beep',
+            [
+                Tone( ("A4", 0.5) ),
+                Tone( ("A5", 0.5) ),
+            ]
+        ),
+        (0x202000, 'Tadah', Tone(
             ("A5", 0.2), 0.2,
             ("B5", 0.2), ("C6", 0.2), 0.2,
             ("D6", 0.2), ("D#6", 0.2)
         ) ),
-        (0x202000, '3', Tone(
+        (0x202000, 'Arpg', Tone(
             ("C6", 0.2), ("E6", 0.2), ("G6", 0.2), ("C7", 0.2),
             ("G6", 0.2), ("E6", 0.2), ("C6", 0.5),
         ) ),
         # 4th row ----------
-        (0x101010, '*', K("A")),
-        (0x800000, '0', K("M")),
-        (0x101010, '#', L("Hello 123")),
+        (0x101010, 'a', K("A")),
+        (0x800000, 'M', K("SHIFT", "M")),
+        (0x101010, 'Hello', Type("Hello 123")),
         # Encoder button ---
         (0x000000, '', onoff)
     ],
-    'enter' : entering,
-    'leave' : leaving,
 }
